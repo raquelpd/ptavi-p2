15bbe89c6e16d69bfaa8efe3c7b5140ebfc07b7b
Raquel Perez Dominguez <raquelpd@gamma19.aulas.gsyc.urjc.es>
2016-09-26 15:41:50 +0200

    Practica2. Apartado6

--- calcplus.py
+++ calcplus.py
@@ -2,12 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import calcoohija
+import calcoo
 
 fich = open('operaciones', 'r')
 operaciones = fich.readlines()
 
 if __name__ == "__main__":	
 
+	calcplus = calcoohija.CalculadoraHija()
+	
 	for operacion in operaciones:
 		operador = operacion.split(',')[0]
 		numeros = operacion.split(',')[1:]
@@ -15,22 +19,22 @@ if __name__ == "__main__":
 		if operador == "suma":
 			print("Suma =") 
 			for i in range(1,len(numeros)):			
-				result = result + int(numeros[i])	
+				result = calcplus.suma(result, int(numeros[i]))
 		elif operador == "resta":
 			print("Resta =") 
 			for i in range(1,len(numeros)):			
-				result = result - int(numeros[i])
+				result = calcplus.resta(result,int(numeros[i]))
 		elif operador == "multiplica":
 			print("Multiplicación =") 
 			for i in range(1,len(numeros)):			
-				result = result * int(numeros[i])
+				result = calcplus.producto(result, int(numeros[i]))
 		elif operador == "divide":
 			print("División =") 
 			for i in range(1,len(numeros)):
-				if numeros[i] == 0:
+				if numeros[i] == "0":
 					result = "division by zero is not allowed"
 				else:
-					result = result / int(numeros[i])
+					result = calcplus.div(result, int(numeros[i]))
 		else:
 			result = "solo suma, resta, multiplica o divide"
 		print(result)
